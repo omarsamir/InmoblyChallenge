bf57f953799d4f33a785526b111808e6effb3730
Omar Samir <omarsamir@live.co.uk>
2018-09-16 23:45:17 +0200

    feat: VIPER Layers

---
+++ inmobly/inmobly/Entity/FlickrResource.swift
@@ -0,0 +1,13 @@
+//
+//  FlickrResource.swift
+//  inmobly
+//
+//  Created by Omar Samir on 9/16/18.
+//  Copyright © 2018 Omar Samir. All rights reserved.
+//
+
+import UIKit
+
+class FlickrResource: NSObject {
+
+}
---
+++ inmobly/inmobly/Presenter/Presenter.swift
@@ -0,0 +1,13 @@
+//
+//  Presenter.swift
+//  inmobly
+//
+//  Created by Omar Samir on 9/16/18.
+//  Copyright © 2018 Omar Samir. All rights reserved.
+//
+
+import UIKit
+
+class Presenter: NSObject {
+
+}
